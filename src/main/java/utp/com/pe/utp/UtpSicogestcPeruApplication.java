9236846895d4b04c09b2865795e6b7e0f1ee615e
Brian Albites <brianleva2728@gmail.com>
2025-08-25 21:19:53 -0500

    first commit

---
+++ src/main/java/utp/com/pe/utp/UtpSicogestcPeruApplication.java
@@ -0,0 +1,13 @@
+package utp.com.pe.utp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtpSicogestcPeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtpSicogestcPeruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utp/com/pe/utp/UtpSicogestcPeruApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.com.pe.utp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtpSicogestcPeruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
